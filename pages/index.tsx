7f2ffe6b664cadc3cbc70ea0497786826b19b215
Yuri <yuribrasiliano37@gmail.com>
2023-04-08 20:42:20 -0300

    Finalizando Login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,36 @@
+import useCurrentUser from "@/hooks/useCurrentUser"
+import { NextPageContext } from "next"
+import { signOut, getSession} from "next-auth/react"
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context)
+
+  if (!session) {
+    return{
+      redirect:{
+        destination: "/auth",
+        permanent: false,
+      }
+    }
+  }
+  return{
+    props: {}
+  }
+  
+}
+
+
 export default function Home() {
+
+  const {data: user} = useCurrentUser();
+
   return (
     <>
     <h1>
       Portal Corpu
     </h1>
+    <p>Salve {user?.name}</p>
+    <button className="h-10 w-full bg-white" onClick={() => signOut()}>Sair</button>
     </>
   )
 }
--- pages/newuser.tsx
+++ pages/newuser.tsx
@@ -1,30 +1,32 @@
 import Input from "@/components/Input";
+import axios from 'axios'
+import { useRouter } from "next/router";
 import { useCallback, useState } from "react";
-import { useRouter } from 'next/router';
-import {signIn} from "next-auth/react"
 
-const Auth = () =>{
 
-    const [email, setEmail] = useState('');
+const NewUser = () =>{
+
+
     const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
 
+    const register = useCallback(async () => {
+        try {
+          await axios.post('/api/register', {
+            email,
+            name,
+            password
+          });
+          
 
-    const login = useCallback(async () => {
-        try{
-            await signIn('credentials',{
-                email,
-                password,
-                redirect: false,
-                callbackUrl: '/'
-            });
-
-            router.push('/')
-        }catch(error){
-            console.log(error)
+          router.push('/auth');
+        } catch (error) {
+            console.log(error);
         }
+      }, [email, name, password, router]);
 
-      }, [email, password, router])
     return(
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-cover
         bg-center bg-fixed">
@@ -33,14 +35,21 @@ const Auth = () =>{
                 lg-max-w-md rounded-[50px] w-full m-auto" >
                     <img src="/images/logo.png" alt="logo" className="h-20 m-auto"/>
                     <h2 className="text-teal-400 text-4xl mb-8 font-semibold mt-[50px]">
-                        Login
+                        Criar Conta
                     </h2>
                     <div className="flex flex-col gap-4">
-                        <Input 
+                    <Input 
                         label="Email"
                         onChange={(ev:any) => setEmail(ev.target.value)}
                         id="email"
+                        type="email"
                         value={email}
+                        />   
+                        <Input 
+                        label="Usuário"
+                        onChange={(ev:any) => setName(ev.target.value)}
+                        id="name"
+                        value={name}
                         />
                         <Input 
                         label="Senha"
@@ -50,7 +59,7 @@ const Auth = () =>{
                         value={password}
                         />                        
                     </div>
-                    <button onClick={login} className="bg-teal-400 py-3 text-white rounded-md w-full mt-10 hover:bg-teal-600
+                    <button onClick ={register} className="bg-teal-400 py-3 text-white rounded-md w-full mt-10 hover:bg-teal-600
                     transition">
                         Entrar
                     </button>
@@ -60,4 +69,4 @@ const Auth = () =>{
     )
 }
 
-export default Auth;
+export default NewUser;
